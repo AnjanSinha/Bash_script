f9408fa0469f5dfcb24a4e309a00226a62244b75
AnjanSinha <sinhaanjan2002@gmail.com>
2022-09-03 18:24:08 +0530

    Adding new script

---
+++ lowerupper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Enter a string to convert in normal form : "
+read a
+
+echo "Enter a string to convert in uppercases: "
+read b
+
+echo "Enter a string to uppercase particular letters : "
+read c 
+
+echo "Normal form of first string is : ${a^} "
+echo "Uppercases of second string is : ${b^^}"
+echo "Converted form of the third string is : ${c^J}"
+
